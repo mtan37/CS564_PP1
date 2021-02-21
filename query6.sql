2beeaa7e5447bd59c2f8e3d1f63737e12a84c791
Zach Brantmeier <brantmeierz@gmail.com>
2021-02-21 15:21:17 -0600

    Update query6.sql

--- query6.sql
+++ query6.sql
@@ -1,1 +1,1 @@
-SELECT COUNT (DISTINCT user_id) FROM User, Bid, Item WHERE User.user_id=Item.seller_id AND User.user_id=Bid.bidder_id;
+SELECT COUNT(DISTINCT user_id) FROM User, Bid, Item WHERE User.user_id=Item.seller_id AND User.user_id=Bid.bidder_id;
