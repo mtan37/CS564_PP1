0d9476421bf64ee486047dd8bca8b77fa28d4195
nicolejzhang <43916876+nicolejzhang@users.noreply.github.com>
2021-02-21 15:34:55 -0600

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,14 +1,3 @@
-WITH "NumDescript" AS (
-    SELECT item_id, COUNT(description) AS "num_d"
-    FROM CATEGORY C
-    GROUP BY item_id
-)
-SELECT COUNT(num_d) AS "result"
-FROM NumDescript N, BID B
-WHERE N.item_id = B.item_id
-  AND num_d = 4;
-
---Marvin's query
 WITH tmp as (
     SELECT c.item_id, count(*) as category_count 
     from Category c
