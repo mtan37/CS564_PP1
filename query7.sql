262162f6cdc82e04f40b8e63dff136b33080e24c
Marvin Tan <marvin.tan@wisc.edu>
2021-02-21 21:22:47 +0000

    update query7

--- query7.sql
+++ query7.sql
@@ -1,1 +1,7 @@
---TODO
+WITH tmp as (
+    SELECT c.description from Category c
+JOIN Item i on c.item_id=i.item_id
+WHERE i.current_price >100.0 and i.num_bids >=1
+GROUP BY c.description
+            )
+SELECT count(*) from tmp;
