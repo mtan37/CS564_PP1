752be4263aa4143a4996d75d1d8519c955c9b16a
nicolejzhang <43916876+nicolejzhang@users.noreply.github.com>
2021-02-21 15:27:34 -0600

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,1 +1,10 @@
---TODO
+WITH "NumDescript" AS (
+    SELECT item_id, COUNT(description) AS "num_d"
+    FROM CATEGORY C
+    GROUP BY item_id
+)
+SELECT COUNT(num_d) AS "result"
+FROM NumDescript N, BID B
+WHERE N.item_id = B.item_id
+  AND num_d = 4
+:
